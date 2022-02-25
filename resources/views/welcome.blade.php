a6763f3fd49040586fdd17eecf0dcb9868f28a78
edson <edsoneduardo526@gmail.com>
2022-02-25 09:47:58 -0300

    Adiciona arquivos estáticos ao Blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,11 @@
       <meta charset="UTF-8">
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <link rel="stylesheet" href="/css/styles.css">
       <title>Bem-Vindo</title>
     </head>
     <body>
       <h1>Boas vindas {{ $nome }}!</h1>
-      <ul>
-        @for($i = 0; $i < count($arr); $i++)
-          <li>{{ $arr[$i] }}</li>
-        @endfor
-        </ul>
+      <img src="/img/banner.jpg" alt="banner" class="banner" />
     </body>
 </html>
