a10626dec8ff585e9bb815ada9c6e0b1e0178458
edson <edsoneduardo526@gmail.com>
2022-02-25 08:54:21 -0300

    Cria rotas e views

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,2 @@
+<h1>Contato aqui</h1>
+<a href="/">Voltar para a home</a>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/contato', function () {
+    return view('contact');
+});
+
+Route::get('/produtos', function () {
+    return view('products');
+});
