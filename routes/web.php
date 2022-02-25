78a392485d1f10570479a035124b4c86149e0466
edson <edsoneduardo526@gmail.com>
2022-02-25 09:35:11 -0300

    Trabalhando com dados no Blade

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $nome = "Edson";
+    $arr = [1, 2, 3, 4, 5];
+    return view('welcome', [
+        'nome' => $nome,
+        'arr' => $arr,
+    ]);
 });
 
 Route::get('/contato', function () {
