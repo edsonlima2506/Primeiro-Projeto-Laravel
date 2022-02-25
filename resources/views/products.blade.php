372d55bc8aa6cf45142eab82ebfb74b1b46846b4
edson <edsoneduardo526@gmail.com>
2022-02-25 10:29:19 -0300

    Cria layout main

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,2 +1,6 @@
+@extends('layouts.main')
+@section('title', 'Produtos')
+@section('footer')
 <h1>Tela de produtos</h1>
 <a href="/">Voltar para a home</a>
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-      <meta charset="UTF-8">
-      <meta http-equiv="X-UA-Compatible" content="IE=edge">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="stylesheet" href="/css/styles.css">
-      <title>Bem-Vindo</title>
-    </head>
-    <body>
-      <h1>Boas vindas {{ $nome }}!</h1>
-      <img src="/img/banner.jpg" alt="banner" class="banner" />
-    </body>
-</html>
+@extends('layouts.main')
+@section('title', 'BLKCD Events')
+
+@section('footer')
+<h1>Boas vindas {{ $nome }}!</h1>
+<img src="/img/banner.jpg" alt="banner" class="banner" />
+@endsection
